717a4bf1d0febc103a0e476aa63cea0855627559
Rick Lamers <ricklamers@gmail.com>
2025-04-15 00:05:47 +0200

    refactor: remove unused imports and redundant code from chatHandler.js and main.js

--- electron/chatHandler.js
+++ electron/chatHandler.js
@@ -1,6 +1,4 @@
 const Groq = require('groq-sdk');
-const fs = require('fs');
-const path = require('path');
 const { pruneMessageHistory } = require('./messageUtils'); // Import pruning logic
 
 /**
